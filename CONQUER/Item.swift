33a61e977d1559b8fa9542f727dfee3c290fe7c4
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2023-12-30 17:46:28 +0400

    Initial Commit

---
+++ CONQUER/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  CONQUER
+//
+//  Created by Abdulaziz Albastaki on 30/12/2023.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
