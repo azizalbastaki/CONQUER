ea1d1866256a7f4dda5dd7678faf97d4674c12be
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2023-12-30 19:25:05 +0400

    work has begun on the todo view

---
+++ CONQUER/todaystodoview.swift
@@ -0,0 +1,18 @@
+//
+//  todaystodoview.swift
+//  CONQUER
+//
+//  Created by Abdulaziz Albastaki on 30/12/2023.
+//
+
+import SwiftUI
+
+struct todaystodoview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    todaystodoview()
+}
