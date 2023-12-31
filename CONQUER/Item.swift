a6db859a95461e0673d2879163af44c3596ebf4b
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2023-12-31 15:54:37 +0400

    SwiftData work

--- CONQUER/Item.swift
+++ CONQUER/Item.swift
@@ -7,22 +7,68 @@
 
 import Foundation
 import SwiftData
+import SwiftUI
 
 @Model
 final class Item {
-    var timestamp: Date
-    
-    init(timestamp: Date) {
-        self.timestamp = timestamp
+    var timestamp: String
+    var itemType: ItemType
+    var journals = [journal]()
+    init(itemType: ItemType) {
+        self.itemType = itemType
+        self.timestamp = getTodaysDate()
     }
 }
 
-@Model
-final class entry {
-    var date: Date
+enum ItemType: Codable, Hashable{
+    case tobelist
+    case dailyEntry
+    case routines
+    case projects
+}
+
+struct journal: Codable, Hashable {
+    var journalTitle: String
+    var journalText: String
+}
+
+class SubTask {
+    var completed: Bool
+    var deadline: Date
+    //    var tag: String
+    //    var tagColor: Color
+    var duration: Int
+    var taskTitle: String
+    var taskDescription: String
     
-    init(date: Date) {
-        self.date = date
+    init(title: String, deadline : Date?, duration: Int?, description: String?) {
+        self.taskTitle = title
+        self.completed = false
+        self.deadline = Date(timeIntervalSinceNow: 3153600000)
+        self.duration = 1
+        self.taskDescription = ""
+        if (deadline != nil) {
+            self.deadline = deadline!
+        }
+        if (duration != nil) {
+            self.duration = duration!
+        }
+        if (description != nil) {
+            self.taskDescription = description!
+        }
     }
+}
+
+class ToDoTask: SubTask {
+    var tag: String
+    var tagColor: Color
+    var subTasks: [SubTask]
     
+    init(title: String, deadline : Date?, duration: Int?, description: String?, tagTitle: String, color: Color) {
+        self.tag = tagTitle
+        self.tagColor = color
+        self.subTasks = []
+        super.init(title: title, deadline: deadline, duration: duration, description: description)
+        
+    }
 }
---
+++ CONQUER/conquerobjects.swift
@@ -0,0 +1,9 @@
+//
+//  conquerobjects.swift
+//  CONQUER
+//
+//  Created by Abdulaziz Albastaki on 31/12/2023.
+//
+
+import Foundation
+import SwiftUI
