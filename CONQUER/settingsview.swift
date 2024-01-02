9d187b902045109410916e9cc68db100ffc76e43
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2024-01-02 11:58:14 +0400

    new settings view, with temporary delete all data button.

---
+++ CONQUER/settingsview.swift
@@ -0,0 +1,18 @@
+//
+//  settingsview.swift
+//  CONQUER
+//
+//  Created by Abdulaziz Albastaki on 02/01/2024.
+//
+
+import SwiftUI
+
+struct settingsview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    settingsview()
+}
