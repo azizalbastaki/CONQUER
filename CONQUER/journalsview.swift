94c994add734aca5fba49f649bc84c0b5ec74c23
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2024-01-05 05:48:17 +0400

    new journal view

---
+++ CONQUER/journalsview.swift
@@ -0,0 +1,18 @@
+//
+//  journalsview.swift
+//  CONQUER
+//
+//  Created by Abdulaziz Albastaki on 04/01/2024.
+//
+
+import SwiftUI
+
+struct journalsview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    journalsview()
+}
