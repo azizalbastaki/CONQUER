8e7aaa5d6145c1b0f67c973ee0cbab27b920e558
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2024-01-16 09:28:27 +0400

    new project view

---
+++ CONQUER/projectview.swift
@@ -0,0 +1,18 @@
+//
+//  projectview.swift
+//  CONQUER
+//
+//  Created by Abdulaziz Albastaki on 16/01/2024.
+//
+
+import SwiftUI
+
+struct projectview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    projectview()
+}
