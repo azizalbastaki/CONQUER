8d3d0253e1d3fdc837800206b28479a95dbf2f5a
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2024-01-17 20:56:47 +0400

    progress on projectview - nothing works yet.

--- CONQUER/projectview.swift
+++ CONQUER/projectview.swift
@@ -6,9 +6,74 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct projectview: View {
+    var ourModelContext: ModelContext
+    @State var entries: [Item]
     var body: some View {
-        Text("Projects")
+        NavigationStack {
+            VStack {
+                HStack{
+                    Text("Projects")
+                        .bold()
+                        .font(.title)
+                        .padding()
+                    Spacer()
+                }
+                
+                List {
+                    ForEach(self.getProjects()) { project in
+                    
+                        NavigationLink {
+                            VStack {
+                                Text(project.timestamp!)
+                                    .bold()
+                                    .font(.title)
+                                    .padding()
+                                Text("Project Documents")
+                                HStack {
+                                    ScrollView(.horizontal) {
+                                        ForEach(project.journals!) { journal in
+                                            NavigationLink {
+                                                VStack {
+                                                    Text(journal.journalTitle)
+                                                        .font(.title)
+                                                        .bold()
+                                                        .padding()
+                                                }
+                                            } label: {
+                                                Text(journal.journalTitle)
+                                                    .background(.red)
+                                                    .clipShape(.capsule)
+                                                    .padding()
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        } label: {
+                            HStack{
+                                Text(project.timestamp!)
+                                    .padding(.horizontal)
+                                Spacer()
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        }
     }
+    
+    func getProjects() -> [Item] {
+        var projects: [Item] = []
+        for entry in entries {
+            if (entry.itemType == .projects) {
+                projects.append(entry)
+            }
+        }
+        return projects
+    }
+
 }
