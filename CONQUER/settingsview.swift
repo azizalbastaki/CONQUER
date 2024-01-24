9c9eab12e70daaeaa65ad859384beb160bc6c340
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2024-01-24 20:48:55 +0400

    updated plan + work on entries display

--- CONQUER/settingsview.swift
+++ CONQUER/settingsview.swift
@@ -5,17 +5,55 @@
 //  Created by Abdulaziz Albastaki on 02/01/2024.
 //
 
+/*
+ //
+ ** TO DO LIST **
+ Add To Be List
+ Add To Be List Journal option
+ Add routines and their options
+ Turn this into a navigation stack
+ View Past ratings system
+ Add option to export all date into
+ 
+ ** POST RELEASE LIST **
+ 
+ -Delete old entries
+ -Change theme
+ -Language
+ -OpenAPI Key Slot
+ */
 import SwiftUI
 import SwiftData
 struct settingsview: View {
     var ourModelContext: ModelContext
     var initalizeConquer: () -> Void
     var body: some View {
-        VStack {
-            Text("Settings")
-            Button(action: deletething, label: {
-                Text("Delete all data and crash")
-            })
+        NavigationStack {
+            VStack {
+                HStack{
+                    Text("Settings")
+                        .bold()
+                        .font(.title)
+                        .padding()
+                    Spacer()
+                }
+                List {
+                    Section("Ratings") {
+                        NavigationLink {
+                            Text("")
+                        } label: {
+                            Text("View ratings from past days")
+                        }
+                        
+                    }
+                    
+                }
+                
+                Text("Settings")
+                Button(action: deletething, label: {
+                    Text("Delete all data and crash")
+                })
+            }
         }
     }
     func deletething() {
@@ -27,7 +65,22 @@ struct settingsview: View {
         }
     }
     
+    func getRatings() {
+        //
+    }
+    
 }
+
+struct ratingItem {
+    var rating: Double
+    var timestamp : String
+    
+    init(rating: Double, timestamp: String) {
+        self.rating = rating
+        self.timestamp = timestamp
+    }
+}
+
 //
 //#Preview {
 //    settingsview()
